f022be207f64ed1e76f4622327d89e712fde0b4f
ftMadrid <fernandomadridcv04@gmail.com>
2025-09-05 08:34:52 -0600

    Primer commit

---
+++ LabWord/src/labword/Main.java
@@ -0,0 +1,11 @@
+package labword;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Que significa el bro respeta.");
+        
+    }
+    
+}
