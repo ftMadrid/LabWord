595360e583058369deae2ad429f02afb9964b6f7
JustPreo <impre0hn@gmail.com>
2025-09-05 10:33:44 -0600

    a

--- LabWord/src/labword/Funciones.java
+++ LabWord/src/labword/Funciones.java
@@ -15,12 +15,23 @@ public class Funciones {
     }
 
     public void cambiarFuente(String fuente) {//La fuente que se usara 
+        MutableAttributeSet attrs = new SimpleAttributeSet();
+        StyleConstants.setFontFamily(attrs, fuente);
+        textPane.setCharacterAttributes(attrs, false);
     }
 
     public void cambiarTamano(int tamano) {//El tamano que se consigue con el boton ese
+        MutableAttributeSet attrs = new SimpleAttributeSet();
+        StyleConstants.setFontSize(attrs, tamano);
+        textPane.setCharacterAttributes(attrs, false);
     }
 
     public void cambiarColor(Color color) {//Con la libreria esa
+        if (color != null) {
+            MutableAttributeSet attrs = new SimpleAttributeSet();//Osea que pueda cambiarl os atributos despues
+            StyleConstants.setForeground(attrs, color);
+            textPane.setCharacterAttributes(attrs, false);
+        }
     }
 
     public void aplicarEstilo() {//Aun no se xd
